efa8c6c2e8f3baa09e4a7aa2243c36d74304ba5c
TristinRohr <161913577+TristinRohr@users.noreply.github.com>
2024-08-26 21:58:20 -0400

    fixed main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import AboutMe from './pages/AboutMe';
-import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
-import Resume from './pages/Resume';
+// import AboutMe from './pages/AboutMe';
+// import Portfolio from './pages/Portfolio';
+// import Contact from './pages/Contact';
+// import Resume from './pages/Resume';
 import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
