cb9a9f8432488dee99cbfa8a897bcfb7d8772bb8
TristinRohr <161913577+TristinRohr@users.noreply.github.com>
2024-08-26 22:21:10 -0400

    fixed pathing

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -9,37 +9,37 @@ const Portfolio = () => {
     <section className='portfolio'>
       <Project
         title="Find My Brewery"
-        image="/src/assets/FMB-logo.webp"
+        image="/FMB-logo.webp"
         deployedLink="https://tristinrohr.github.io/Find-My-Brewey/"
         repoLink="https://github.com/TristinRohr/Find-My-Brewey"
         />
       <Project
         title="Potty Partner"
-        image="/src/assets/PP-logo.png"
+        image="/PP-logo.png"
         deployedLink="https://potty-partner.onrender.com/"
         repoLink="https://github.com/ndzamboni/Potty_Partner"
       />
       <Project
         title="Live LOL Tracker"
-        image="/src/assets/LLT-logo.jpeg"
+        image="/LLT-logo.jpeg"
         deployedLink=""
         repoLink="https://github.com/TristinRohr/Live-LOL-Tracker"
       />
       <Project
         title= "Task Manager"
-        image= "/src/assets/TMlogo.webp"
+        image= "/TMlogo.webp"
         deployedLink= "https://tristinrohr.github.io/Task-Manager-Project-5/"
         repoLink= "https://github.com/TristinRohr/Task-Manager-Project-5"
       />
       <Project
         title= "Text Editor"
-        image= "/src/assets/JATE-logo.png"
+        image= "/JATE-logo.png"
         deployedLink= "https://text-editor-jby0.onrender.com/"
         repoLink= "https://github.com/TristinRohr/text-editor"
       />
       <Project
         title= "Weather Dashboard"
-        image= "/src/assets/WD-logo.webp"
+        image= "/WD-logo.webp"
         deployedLink= "https://tristinrohr.github.io/weather/"
         repoLink= "https://github.com/TristinRohr/weather"
       />
