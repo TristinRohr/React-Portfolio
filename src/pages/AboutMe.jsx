229b7bde9ef53c31bda9c17b2071d71f15a904d6
TristinRohr <161913577+TristinRohr@users.noreply.github.com>
2024-09-19 13:21:16 -0400

    typo

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -7,7 +7,7 @@ const AboutMe = () => {
       <h2>About Me</h2>
       <img src="/profilecopy.jpeg" alt="Tristin Rohr" />
       <p>Tristin Rohr - Full-Stack Web Developer</p>
-      <p>I am an aspiring Full-Stack Web Developer with a passion for creating dynamic and responsive web applications. Recently completed in a comprehensive Full-Stack Web Development Bootcamp at UNC-Chapel Hill, I am honing my skills in both front-end and back-end technologies. My background in various industries, from solar consulting to retail and food service, has equipped me with strong problem-solving abilities, excellent communication skills, and a commitment to teamwork. I am eager to apply my knowledge and creativity to contribute to a dynamic development team. Let’s connect and build something great together!</p>
+      <p>I am an aspiring Full-Stack Web Developer with a passion for creating dynamic and responsive web applications. Recently completed a comprehensive Full-Stack Web Development Bootcamp at UNC-Chapel Hill, I am honing my skills in both front-end and back-end technologies. My background in various industries, from solar consulting to retail and food service, has equipped me with strong problem-solving abilities, excellent communication skills, and a commitment to teamwork. I am eager to apply my knowledge and creativity to contribute to a dynamic development team. Let’s connect and build something great together!</p>
     </section>
   );
 };
